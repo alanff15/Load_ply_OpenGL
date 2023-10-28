aaaf6f27b57afa3162696fbaeec508762584f597
Alan <alanff15@gmail.com>
2023-10-28 09:33:02 -0300

    Fix position

--- res/app.glsl
+++ res/app.glsl
@@ -1,7 +1,7 @@
 // static const std::string GLSL_STR = R"glsl(#shader vertex
 #version 330 core
 
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 
 out vec3 fcolor;
@@ -9,7 +9,7 @@ out vec3 fcolor;
 uniform mat4 u_mvp;
 
 void main() {
-  gl_Position = u_mvp * position;
+  gl_Position = u_mvp * vec4(position, 1.0);
   fcolor = color;
 }
 
