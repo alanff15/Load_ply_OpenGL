1b36274f06535439a68cc8f4dd7d090464e1be5b
Alan <alanff15@gmail.com>
2023-11-11 09:58:55 -0300

    fix shader

--- res/app.glsl
+++ res/app.glsl
@@ -1,4 +1,4 @@
-// static const std::string GLSL_STR = R"glsl(#shader vertex
+// static const std::string GLSL_STR = R"glsl(//#shader vertex
 #version 330 core
 
 layout(location = 0) in vec3 position;
@@ -16,9 +16,9 @@ void main() {
   ftexCoord = texCoord;
 }
 
-#-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - #
+//-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --//
 
-#shader fragment
+//#shader fragment
 #version 330 core
 
 layout(location = 0) out vec4 fragColor;
@@ -36,14 +36,14 @@ vec3 getLight(vec3 cam, vec3 L, vec3 N, vec3 color) {
   float diffuse = 0.80 * clamp(dot(L, N), 0.0, 1.0);
   float specular = 0.80 * pow(clamp(dot(R, V), 0.0, 1.0), 15.0);
   float fresnel = 0.20 * pow(1.0 + dot(cam, N), 3.0);
-  return (ambient + fresnel + diffuse) * mix(color, lightColor, 0.02) + specular * lightColor;
+  return (ambient + fresnel + diffuse) * color + specular * lightColor;
 }
 
 void main() {
-  vec3 luz = normalize(vec3(0.0, 0.0, -1.0));
-  vec4 cor = texture2D(u_texture, ftexCoord);
   vec3 cam = vec3(0, 0, 1);
-  fragColor = vec4(getLight(cam, luz, normalize(fnormal), cor.xyz), cor.w);
+  vec3 light = normalize(vec3(0.0, 0.0, -1.0));
+  vec4 color = texture2D(u_texture, ftexCoord);
+  fragColor = vec4(getLight(cam, light, normalize(fnormal), color.rgb), color.a);
 }
 
   // )glsl";
